9918b5dd41379fa3684c1a7637bef5755f85312b
Sisir Ahmed <ahnafsadid2025@gmail.com>
2026-07-12 01:43:46 +0600

    First Day Python

---
+++ first.py
@@ -0,0 +1,25 @@
+print ("Hello World" ,"sadid AHmed")
+print (23)
+print (25)
+print (35+23)
+
+name = "sisir"
+age=23
+price = 25.99
+
+age2= age
+print (name)
+print (age)
+print (price)
+print ("my name is ", name)
+print ("my age is", age)
+print (age2)
+print(type (name))
+print (type (age))
+print (type(price))
+name1= "sisir"
+name2 ='sisir'
+name3= '''sisir'''
+
+old = True
+print (type (old))
---
+++ firstPorgram.py
@@ -0,0 +1,23 @@
+# a=100
+# b=4
+# diff = a-b
+# print (diff)
+
+#logical operators
+# print(not False)
+a=50
+b=30
+
+print(not (a>b))
+
+val1=True
+val2=False
+print ("and operator :", val1 and val2) #duitai true hole true noyto false
+print ("or operator :", val1 or val2) #jekono ekta true hoilei hoise
+print ("or operator:" , (a==b)or (a>b))
+
+
+input("My name is" )
+
+val=int (input ("enter the value is:"))
+print(type(val), val)
