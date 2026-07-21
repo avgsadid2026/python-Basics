7aa3ec2524b25dcb4308b7b824ad6d34a1cd805a
Sisir Ahmed <ahnafsadid2025@gmail.com>
2026-07-21 15:48:34 +0600

    third day

---
+++ third_day_python.py
@@ -0,0 +1,38 @@
+#list
+# marks =[94, 95, 97 ,98]
+# print (marks)
+# print (len(marks))
+# print(marks[0] )
+
+# student= ["sisir", 95.5, 17, "Delhi"]
+# print(student)
+
+# #list is mutable(change kora jay , but string change kora jay na)
+# student[0]="Sadid"
+# print(student)
+
+
+# #list slicing ee ending Index Included Hoy na--
+# marks=[85,94,76,48]
+# print(marks[1:3]) #last er agerta porjonto gelo
+
+
+# list Methods(functions)# ei method gulo list kei change korbe kono kisu return korbe na
+# list=[2,1,3]
+# list.append(4)
+# print(list )
+# list.sort()#original string ee gie change,tai print korar somoy kono kisu return kore na
+# print(list)
+# list.sort(reverse=True) #reverse order ee print korte hoy
+# print(list)
+# list.reverse()#puratakei reverse korbe
+# print(list)
+# list.insert(1,5)
+# print(list)
+# list =[2,1,3,1]
+# # list.remove(1)#first one koto age ashtese(first Occurance)
+# list.pop(2)
+# print(list)
+
+
+
